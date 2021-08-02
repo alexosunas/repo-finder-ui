58ac2d54fa5a487b65dd362f06461d9ec9f95317
alexosunas <alex.osunas@gmail.com>
2021-08-01 23:28:09 -0700

    add aws url to test from local

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-    baseURL: "http://localhost:3000/dev",
+    baseURL: "https://gain5ozhcl.execute-api.us-east-1.amazonaws.com/dev",
     headers: {
         "Content-type": "application/json",
     },
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,12 @@ module.exports = {
   devServer: {
     port: 8081
   }
-}
+};
+
+// '^/api': {
+//   target: 'http://localhost:3000',
+//     changeOrigin: true,
+//     secure:false,
+//     pathRewrite: {'^/api': '/api'},
+//   logLevel: 'debug'
+// }
